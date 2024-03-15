e38953e2ea06d87391bd25744159fcbaf4d245ff
KateBushka <101467790+KateBushka@users.noreply.github.com>
2024-03-15 10:14:28 +0300

    Task completed 3 module

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,21 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
+    const arrPath = path.split('.');
 
+    const getter = (obj) => {
+        const l = arrPath.length;
+        let i = 0;
+        let result = undefined;
+        if (l === 0) return result;
+        result = obj[arrPath[i]];
+        i++;
+        while (i < l) {
+            if (!result) return undefined;
+            result = result[arrPath[i]]
+            i++;
+        } 
+        return result;
+    }
+    return getter
 }
