918715dbe6f28787a01d3ae75fb7731ea2d9caf7
KateBushka <101467790+KateBushka@users.noreply.github.com>
2024-03-18 18:38:10 +0300

    module 5-1

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,60 @@
 export default class NotificationMessage {
+    element;
+    lastMessage;
+
+    constructor(
+        message = '',
+        {
+            duration = 0,
+            type = '',
+        } = {}) {
+        this.duration = duration;
+        this.type = type;
+        this.message = message;
+
+        this.element = this.createElement(this.createTemplate());
+    }
+
+    show(container = document.body) {
+        if (NotificationMessage.lastMessage) {
+            NotificationMessage.lastMessage.remove();
+          } 
+          NotificationMessage.lastMessage = this;
+
+        this.timerId = setTimeout(() => {
+            this.destroy();
+        }, this.duration);
+    
+        container.appendChild(this.element);    
+    }
+
+    createElement(template) {
+        const element = document.createElement('div');
+        element.innerHTML = template;
+        return element.firstElementChild;
+    }
+    
+    createTemplate() {
+        return (`  
+            <div class="notification ${this.type}" style="--value:${this.duration}">
+                <div class="timer"></div>
+                <div class="inner-wrapper">
+                    <div class="notification-header">${this.type}</div>
+                    <div class="notification-body">
+                        ${this.message}
+                    </div>
+                </div>
+            </div>
+        `);
+    }
+
+    remove() {
+        this.element.remove();
+    }
+
+    destroy() {
+        this.remove();
+        clearTimeout(this.timerId);
+    }
 
 }
